caf81ba20de28013ab3317b951b7ed2b85808cde
Nota0515 <businessbro78@gmail.com>
2025-11-05 19:52:25 +0530

    made some initial stuff

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,20 @@
-import Button from '../ui/Button';
 import { navLinks } from '@/constants'
 import PixelBlast from '../ui/PixelBast'
+import ShinyText from '../ui/shinytext';
+import ScrambledText from '../ui/ScrambledText';
 import LightRays from '../ui/LightRays';
+
 
 const Home = () => {
   return (
-    <div className="bg-black relative w-screen min-h-[100vh]">
-      <div style={{ width: '100%', height: '100%', position: 'absolute', }}>
+    <div className="bg-black flex flex-col justify-start mx-auto relative min-h-[100vh] overflow-hidden">
+      {/*backgournd */}
+      <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, opacity: 0.2, zIndex: 0 }}>
         <PixelBlast
           variant="circle"
           pixelSize={1.3}
           color="#d0c2ff"
+          liquid
           patternScale={4}
           patternDensity={1.5}
           pixelSizeJitter={0.5}
@@ -19,9 +23,8 @@ const Home = () => {
           transparent
         />
       </div>
-      import LightRays from './LightRays';
 
-      <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, zIndex: 0 }}>
+      <div style={{ width: '100%', opacity: 0.7, height: '100%', position: 'absolute', top: 0, zIndex: 0 }}>
         <LightRays
           raysOrigin="top-center"
           raysColor="#d0c2ff"
@@ -35,16 +38,48 @@ const Home = () => {
           className="custom-rays"
         />
       </div>
-      <div className='navbar w-full'>
-        <div className="flex items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div className='navbar flex justify-center w-full z-20'>
+        <div className="flex items-center justify-center p-4 sm:px-6 lg:px-8">
           <div className='sub-nav-link max-w-full overflow-x-auto'>
             <nav className='flex justify-between items-center text-white'>
-              <div className="nav-links flex items-center bg-white/10 justify-between rounded-full border border-white/20 backdrop-blur-xl drop-shadow-1 gap-2 sm:gap-4 md:gap-6 lg:gap-10 py-2 px-4 sm:px-6 md:px-8 lg:px-10 h-full font-mainFont text-sm whitespace-nowrap">
+              <div className="nav-links shiny-border flex items-center justify-between rounded-full backdrop-blur-xl drop-shadow-1 gap-2 py-2 px-4 sm:px-6 h-full font-mainFont text-sm whitespace-nowrap">
                 {navLinks.map((nav) => (
-                  <a href={nav.link} key={nav.id} className="px-2 sm:px-0 hover:text-purple-400 transition-colors">{nav.name}</a>
+                  <a
+                    href={nav.link}
+                    key={nav.id}
+                    className="px-2 sm:px-3 hover:text-purple-400 transition-colors text-sm"
+                  >
+                    {nav.name}
+                  </a>
                 ))}
               </div>
+
             </nav>
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-center z-10 items-center mx-auto mt-16 max-w-4xl'>
+        <div className='maintitlediv flex justify-center items-center flex-col'>
+          <div className='introducing'>
+            <ShinyText
+              text="Introducing 🎉"
+              disabled={false}
+              speed={2}
+              className='font-mainFont'
+            />
+          </div>
+          <div className='landing-name p-4'>
+            <div className='headingtext font-mainFont text-5xl sm:text-8xl lg:text-9xl xl:text-[12rem] text-white'>
+              <ScrambledText
+              className='dottedom'
+              radius={100}
+              duration={1.2}
+              speed={0.5}
+              scrambleChars={.:}
+              >
+                Seebysound
+              </ScrambledText>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/ui/shinytext.jsx
@@ -0,0 +1,14 @@
+// @ts-ignore: Allow importing CSS as a side-effect without type declarations
+import './ShinyText.css';
+
+const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <div className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`} style={{ animationDuration }}>
+      {text}
+    </div>
+  );
+};
+
+export default ShinyText;
